6a2414aad0a7810bfb7d6d38f2e6a9d998f31a6e
Tibo <tibo_hulens@hotmail.com>
2014-03-02 23:24:38 +0100

    Ik denk dat het wel werkt

--- Sockets/src/Main.java
+++ Sockets/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Dit is een testje");
 		System.out.println("Dit is een testje");
+		System.out.println("Dit is een testje");
 
 	}
 
