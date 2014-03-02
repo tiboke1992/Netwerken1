f9081b38d6e4fa362268bc937aca88cdeaf2a180
Tibo <tibo_hulens@hotmail.com>
2014-03-02 23:44:10 +0100

    laatste test dan slape

--- Sockets/src/Main.java
+++ Sockets/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		System.out.println("Dit is een testje");
 		System.out.println("Dit is een testje");
 		System.out.println("Dit is een testje");
+		System.out.println("Dit is een testje");
 
 	}
 
